d0e16206b609c91ac647741a9638e5f2b9fd8e53
dhcl8881 <hspss9425358@gmail.com>
2025-08-20 00:42:10 +0800

    new

--- liver1/main.py
+++ liver1/main.py
@@ -1,55 +1,88 @@
 import cv2
 import numpy as np
-from utils import sortPoints,sort_coordinates
-green_lower = np.array([36, 25, 25], np.uint8)
-green_upper = np.array([100, 255, 255], np.uint8)
-img = cv2.imread('2.jpg')
-hsvFrame = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-mask = cv2.inRange(hsvFrame, green_lower, green_upper)             
-output = cv2.bitwise_and(img, img, mask = mask ) 
-#output = cv2.cvtColor(output, cv2.COLOR_HSV2BGR)
-gray = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
-contours, _ = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-#print(contours)
-new_contours=[]
-for contour in contours:
-    area = cv2.contourArea(contour)
-    if area>10:
-        new_contours.append(contour)
 
-allcon = np.vstack(new_contours)
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--mix_dir')
+args = parser.parse_args()
 
-epli = 0.0005*cv2.arcLength(allcon,True)
-appr = cv2.approxPolyDP(allcon,epli,True)
-cnt=[]
-for i in appr:
-    cnt.append(i)
-indice = sort_coordinates(appr)
-ind = []
-for i in indice:
-    ind.append(i.reshape(-1,2))
-new_cnt = sortPoints(cnt)
-new_cnt = np.array(new_cnt).astype(np.int32)
-ind = np.vstack(ind).reshape(-1,1,2)
-#output = cv2.drawContours(output,allcon,-1,[0,255,0])
-output = cv2.drawContours(output,[appr],-1,[0,255,0])
-#mask = np.zeros_like(img,dtype=np.uint8)
+if __name__ == '__main__':
+    green_lower = np.array([36, 25, 25], np.uint8)
+    green_upper = np.array([100, 255, 255], np.uint8)
+    img = cv2.imread(args.mix_dir)
+    hsvFrame = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsvFrame, green_lower, green_upper)             
+    output = cv2.bitwise_and(img, img, mask = mask ) 
+    #output = cv2.cvtColor(output, cv2.COLOR_HSV2BGR)
+    gray = cv2.cvtColor(output, cv2.COLOR_BGR2GRAY)
+    contours, _ = cv2.findContours(gray, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    #print(contours)
+    new_contours=[]
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if area>20:
+            new_contours.append(contour)
 
-#cv2.fillPoly(mask,[new_cnt],(255,255,255))
-# 循環遍歷所有輪廓
-for contour in contours:
-    # 計算包圍輪廓的最小矩形
-    x, y, w, h = cv2.boundingRect(contour)
-    hull = cv2.convexHull(contour)
-    length = len(hull)
-    #for i in range(len(hull)):
-        #cv2.line(output, tuple(hull[i][0]), tuple(hull[(i+1)%length][0]), (0,255,0), 2)
-    #output = cv2.drawContours(output,contour,-1,[0,255,0])
-    # 繪製矩形框
-    #cv2.rectangle(output, (x, y), (x+w, y+h), (0, 255, 0), 2)
+    allcon = np.vstack(new_contours)
+    allcon1 = sorted(allcon , key=lambda k: [k[0][0], k[0][1]])
+    
+    x=0
+    y1=0
+    y2=0
+    n_list=[]
+    for i in allcon1:
+        if x==0 and y1==0:
+            x = i[0][0]
+            y1 = i[0][1]
+            y2 = i[0][1]
+        if x==i[0][0] and y2<i[0][1] :
+            y2 = i[0][1]
+        elif x==i[0][0] and y1>i[0][1] :
+            y1 = i[0][1]
+        elif x!=i[0][0] and y1!=y2:
+            n_list.append([[int(x),int(y1)]])
+            n_list.append([[int(x),int(y2)]])
+            x = i[0][0]
+            y1 = i[0][1]
+            y2 = i[0][1]
+        elif x!=i[0][0] and y1==y2 :
+            n_list.append([[int(x),int(y1)]])
+            #n_list.append([[int(x),int(y2)]])
+            x = i[0][0]
+            y1 = i[0][1]
+            y2 = i[0][1]
+    import math
 
-    # 輸出寬度和高度
-    print(f"寬度: {w}, 高度: {h}")
-cv2.imwrite('output.jpg', output)
-cv2.waitKey(0)                                   
-cv2.destroyAllWindows()
+    def euclidean_distance(p1, p2):
+        return math.sqrt((p1[0][0] - p2[0][0])**2 + (p1[0][1] - p2[0][1])**2)        
+    nn_list=[]
+    nn_list.append(n_list[0])
+    while len(n_list)!=0:
+        reference_point = nn_list[-1]
+        n_list.remove(reference_point)
+        sorted_points = sorted(n_list, key=lambda p: euclidean_distance(p, reference_point))
+        if sorted_points!=[]:
+            nn_list.append(sorted_points[0])
+    X_new = np.array(nn_list)
+
+    output = cv2.drawContours(output,[allcon],-1,[0,255,0],thickness=1)
+    #mask = np.zeros_like(img,dtype=np.uint8)
+
+    #cv2.fillPoly(mask,[new_cnt],(255,255,255))
+    # 循環遍歷所有輪廓
+    for contour in contours:
+        # 計算包圍輪廓的最小矩形
+        x, y, w, h = cv2.boundingRect(contour)
+        hull = cv2.convexHull(contour)
+        length = len(hull)
+        #for i in range(len(hull)):
+            #cv2.line(output, tuple(hull[i][0]), tuple(hull[(i+1)%length][0]), (0,255,0), 2)
+        #output = cv2.drawContours(output,contour,-1,[0,255,0])
+        # 繪製矩形框
+        #cv2.rectangle(output, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+        # 輸出寬度和高度
+        print(f"寬度: {w}, 高度: {h}")
+    cv2.imwrite('output.jpg', output)
+    cv2.waitKey(0)                                   
+    cv2.destroyAllWindows()
